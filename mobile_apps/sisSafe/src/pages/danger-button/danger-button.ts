77e5020b6ac20cc0ae5a88b07ccc36cdb3f0cea7
Himanshu Gupta <himanshugupta.396@gmail.com>
2017-10-04 01:16:48 +0530

    packages updated, ionic updated to 3.12.0, get location of device feature integrated using google maps sdk, platform android added + splash + icon , app_id for SheSafe added

--- mobile_apps/sisSafe/src/pages/danger-button/danger-button.ts
+++ mobile_apps/sisSafe/src/pages/danger-button/danger-button.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 /**
  * Generated class for the DangerButtonPage page.
@@ -13,8 +15,24 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'danger-button.html',
 })
 export class DangerButtonPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  lat:any;
+  long:any;
+  location_link:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private geolocation: Geolocation) {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat=  resp.coords.latitude;
+      this.long = resp.coords.longitude;
+      this.location_link = 'https://www.google.com/maps/?q=' + this.lat + ',' + this.long;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+     });
   }
 
   ionViewDidLoad() {
