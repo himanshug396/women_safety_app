b8dccfff63e2fd93650833050b01fd05976da57e
Himanshu Gupta <himanshugupta.396@gmail.com>
2017-10-15 12:43:36 +0530

    final work

--- mobile_apps/sisSafe/src/app/app.module.ts
+++ mobile_apps/sisSafe/src/app/app.module.ts
@@ -8,30 +8,17 @@ import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
 import { MapsNearbyPage } from '../pages/maps-nearby/maps-nearby';
-import { MapsNearbyBankPage } from '../pages/maps-nearby-bank/maps-nearby-bank';
-import { MapsNearbyMedicalPage } from '../pages/maps-nearby-medical/maps-nearby-medical';
-import { MapsNearbyTaxiPage } from '../pages/maps-nearby-taxi/maps-nearby-taxi';
-import { MapsNearbyStationPage } from '../pages/maps-nearby-station/maps-nearby-station';
-import {TermsAndConditionsPage} from '../pages/terms-and-conditions/terms-and-conditions';
 import { ListNearbyPage } from '../pages/list-nearby/list-nearby';
-import { HomeNearbyBankPage } from '../pages/home-nearby-bank/home-nearby-bank';
+
 import { HomeNearbyPage } from '../pages/home-nearby/home-nearby';
-import { HomeNearbyMedicalPage } from '../pages/home-nearby-medical/home-nearby-medical';
-import { HomeNearbyStationPage } from '../pages/home-nearby-station/home-nearby-station';
-import { HomeNearbyTaxiPage} from '../pages/home-nearby-taxi/home-nearby-taxi';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import {KnowThisLocalityPage} from '../pages/know-this-locality/know-this-locality';
 import {NearestWhatPage} from '../pages/nearest-what/nearest-what';
 import {LocalityDetailPage} from '../pages/locality-detail/locality-detail';
 import { ConnectivityProvider } from '../providers/connectivity/connectivity';
 import { GoogleMapsProvider } from '../providers/google-maps/google-maps';
-import { LocationPage } from '../pages/location/location';
 
 import { LocationsProvider } from '../providers/locations/locations';
-import { ClickUploadPage } from '../pages/click-upload/click-upload';
-import { LoginPage} from '../pages/login/login';
-import { VerifyOtpPage} from '../pages/verify-otp/verify-otp';
-import {AddContactsPage} from '../pages/add-contacts/add-contacts';
 import { Network } from '@ionic-native/network';
 import { Geolocation } from '@ionic-native/geolocation';
 
@@ -42,13 +29,30 @@ import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
 import { Contacts} from '@ionic-native/contacts';
 
-import { HomePopOverPage} from '../pages/home-pop-over/home-pop-over';
-import { NetworkErrorPage} from '../pages/network-error/network-error';
 import { ShesafeBackendProvider} from '../providers/shesafe-backend/shesafe-backend';
-import { ContactUsPage} from '../pages/contact-us/contact-us';
-import { AddReviewPage  } from '../pages/add-review/add-review';
 import { ExpandableHeaderComponent } from '../components/expandable-header/expandable-header';
 import { Ionic2RatingModule } from 'ionic2-rating';
+
+
+import { LoginPageModule} from '../pages/login/login.module';
+import { VerifyOtpPageModule} from '../pages/verify-otp/verify-otp.module';
+import {AddContactsPageModule} from '../pages/add-contacts/add-contacts.module';
+import {AddReviewPageModule} from '../pages/add-review/add-review.module';
+import { HomeNearbyBankPageModule } from '../pages/home-nearby-bank/home-nearby-bank.module';
+import { HomeNearbyMedicalPageModule } from '../pages/home-nearby-medical/home-nearby-medical.module';
+import { HomeNearbyStationPageModule } from '../pages/home-nearby-station/home-nearby-station.module';
+import { HomeNearbyTaxiPageModule} from '../pages/home-nearby-taxi/home-nearby-taxi.module';
+import { ClickUploadPageModule } from '../pages/click-upload/click-upload.module';
+import { ContactUsPageModule} from '../pages/contact-us/contact-us.module';
+import { HomePopOverPageModule} from '../pages/home-pop-over/home-pop-over.module';
+import { NetworkErrorPageModule} from '../pages/network-error/network-error.module';
+import { LocationPageModule } from '../pages/location/location.module';
+import { MapsNearbyBankPageModule } from '../pages/maps-nearby-bank/maps-nearby-bank.module';
+import { MapsNearbyMedicalPageModule } from '../pages/maps-nearby-medical/maps-nearby-medical.module';
+import { MapsNearbyTaxiPageModule } from '../pages/maps-nearby-taxi/maps-nearby-taxi.module';
+import { MapsNearbyStationPageModule } from '../pages/maps-nearby-station/maps-nearby-station.module';
+import {TermsAndConditionsPageModule} from '../pages/terms-and-conditions/terms-and-conditions.module';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -59,14 +63,33 @@ import { Ionic2RatingModule } from 'ionic2-rating';
     KnowThisLocalityPage,
     NearestWhatPage,
     LocalityDetailPage,
-    ExpandableHeaderComponent
+    ExpandableHeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot(),
-    Ionic2RatingModule
+    Ionic2RatingModule,
+    LoginPageModule,
+    VerifyOtpPageModule,
+    AddContactsPageModule,
+    HomeNearbyBankPageModule,
+    HomeNearbyMedicalPageModule,
+    HomeNearbyStationPageModule,
+    HomeNearbyTaxiPageModule,
+    MapsNearbyBankPageModule,
+    MapsNearbyMedicalPageModule,
+    MapsNearbyStationPageModule,
+    MapsNearbyTaxiPageModule,
+    HomePopOverPageModule,
+    ClickUploadPageModule,
+    NetworkErrorPageModule,
+    LocationPageModule,
+    ContactUsPageModule,
+    AddReviewPageModule,
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -78,24 +101,6 @@ import { Ionic2RatingModule } from 'ionic2-rating';
     KnowThisLocalityPage,
     NearestWhatPage,
     LocalityDetailPage,
-    LoginPage,
-    VerifyOtpPage,
-    AddContactsPage,
-    HomeNearbyBankPage,
-    HomeNearbyMedicalPage,
-    HomeNearbyStationPage,
-    HomeNearbyTaxiPage,
-    MapsNearbyBankPage,
-    MapsNearbyMedicalPage,
-    MapsNearbyStationPage,
-    MapsNearbyTaxiPage,
-    HomePopOverPage,
-    ClickUploadPage,
-    NetworkErrorPage,
-    LocationPage,
-    ContactUsPage,
-    AddReviewPage,
-    TermsAndConditionsPage,
   ],
   providers: [
     StatusBar,
